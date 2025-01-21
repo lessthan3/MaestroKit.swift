31b3d15d0d0d0c964e016d05b9ffa7ecd3060395
William Buck <hello@williambuck.dev>
2025-01-21 13:09:25 -0500

    initial SPM release

---
+++ Frameworks/MaestroKit.xcframework/tvos-arm64_x86_64-simulator/MaestroKit.framework/Headers/MaestroKit.h
@@ -0,0 +1,18 @@
+//
+//  MaestroKit.h
+//  MaestroKit
+//
+//  Created by Todd Gibbons on 10/2/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MaestroKit.
+FOUNDATION_EXPORT double MaestroKitVersionNumber;
+
+//! Project version string for MaestroKit.
+FOUNDATION_EXPORT const unsigned char MaestroKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MaestroKit/PublicHeader.h>
+
+
--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "MaestroKit.swift",
+    platforms: [
+        .tvOS(.v16)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -16,13 +19,9 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "MaestroKit.swift",
-            dependencies: []),
-        .testTarget(
-            name: "MaestroKit.swiftTests",
-            dependencies: ["MaestroKit.swift"]),
+        .binaryTarget(
+                name: "MaestroKit.swift",
+                path: "./Frameworks/MaestroKit.xcframework"
+        )
     ]
 )
